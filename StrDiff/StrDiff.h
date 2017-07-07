330a87fd139e1931dc754d2d5a32b47ee7dea0d9
cyun177 <cyun177@gmail.com>
2017-07-07 23:24:15 +0900

    Add new project

---
+++ StrDiff/StrDiff.h
@@ -0,0 +1,19 @@
+//
+//  StrDiff.h
+//  StrDiff
+//
+//  Created by Motonari Nakashima on 2017/07/07.
+//  Copyright © 2017年 motoshima1150. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StrDiff.
+FOUNDATION_EXPORT double StrDiffVersionNumber;
+
+//! Project version string for StrDiff.
+FOUNDATION_EXPORT const unsigned char StrDiffVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StrDiff/PublicHeader.h>
+
+
